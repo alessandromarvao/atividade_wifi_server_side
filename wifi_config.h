e31f61d5b10d1d33ccc5c0814af1f25d7580f513
Alessandro Marvão Nascimento <alessandromarvao@gmail.com>
2025-05-22 19:09:35 -0300

    Update wifi_config.h

--- wifi_config.h
+++ wifi_config.h
@@ -2,8 +2,8 @@
 #define WIFI_CONFIG_H
 
 // Credenciais da rede Wi-Fi
-#define WIFI_SSID "TROJAN:WIN32 4G"
-#define WIFI_PASS "@1153.casA"
+#define WIFI_SSID ""
+#define WIFI_PASS ""
 
 // IP e porta do servidor de destino
 #define SERVER_IP "192.168.4.1"
